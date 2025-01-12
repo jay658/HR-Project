bace70ff8a712ff1698531411813797aa47c649c
Jiayu Zhang <jiayuzhangg@gmail.com>
2025-01-12 14:12:22 -0500

    Added ability to get facility issue by ID, apartment ID, and user ID

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -1,14 +1,16 @@
-import apartmentRouter from './routers/apartmentRouter'
-import express from 'express'
-import onboardingRouter from './routers/onboardingRouter'
-import userRouter from './routers/userRouter'
-import visaRouter from './routers/visaRouter'
+import apartmentRouter from './routers/apartmentRouter';
+import express from 'express';
+import facilityIssueRouter from './routers/facilityRouter';
+import onboardingRouter from './routers/onboardingRouter';
+import userRouter from './routers/userRouter';
+import visaRouter from './routers/visaRouter';
 
-const apiRouter = express.Router()
+const apiRouter = express.Router();
 
-apiRouter.use('/user', userRouter)
-apiRouter.use('/apartment', apartmentRouter)
-apiRouter.use('/onboarding', onboardingRouter)
-apiRouter.use('/visa', visaRouter)
+apiRouter.use('/user', userRouter);
+apiRouter.use('/apartment', apartmentRouter);
+apiRouter.use('/onboarding', onboardingRouter);
+apiRouter.use('/visa', visaRouter);
+apiRouter.use('/facilityIssue', facilityIssueRouter);
 
-export default apiRouter
+export default apiRouter;
