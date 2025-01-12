ac9b71ae6b63a2fe44c4b71e0073d35244ef26ee
JerlsonSik <jlsonxyue@gmail.com>
2025-01-12 13:11:49 -0500

    commit1

--- server/src/api/routers/userRouter.ts
+++ server/src/api/routers/userRouter.ts
@@ -5,6 +5,7 @@ const userRouter = express.Router()
 userRouter.get('/test', (_req, res) => {
   try{
     res.json('Successfully hit user router')
+    console.log("Teseting ")
   }catch(err){
     console.log(`There was an error in the user test route: ${err}`)
   }
