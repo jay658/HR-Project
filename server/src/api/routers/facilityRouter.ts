854de48d008013f6d1a783826d43ffa550079089
Jiayu Zhang <jiayuzhangg@gmail.com>
2025-01-13 20:59:02 -0500

    Separated controllers

--- server/src/api/routers/facilityRouter.ts
+++ server/src/api/routers/facilityRouter.ts
@@ -1,128 +1,24 @@
-import express, { Request, Response } from 'express';
+import {
+  closeFacilityIssue,
+  commentOnFacilityIssue,
+  createFacilityIssue,
+  getFacilityIssue,
+  getFacilityIssuesForApartment,
+  getIssuesForUser,
+  testFacilityRouter
+} from '../../controllers/facility';
 
-import EmployeeUser from '../../models/EmployeeUser';
-import FacilityIssue from '../../models/FacilityIssue';
+import express from 'express';
 
 const facilityIssueRouter = express.Router();
 
-type IssueDetails = {
-  title: string,
-  description: string
-}
-
 facilityIssueRouter
-  .get('/test', (_req, res) => {
-    try{
-      res.json('Successfully hit facility router');
-    }catch(err){
-      console.log(`There was an error in the facility test route: ${err}`);
-    }
-  })
-  .get('/user', async (_req: Request, res: Response) => {
-    try{
-      // When auth is added, we should get the user info from the JWT token. For now, this is to simulate receiving a user id
-      const testUser = await EmployeeUser.findOne({ username: 'john.doe' });
-      const id = testUser?._id;
-
-      const facilityIssues = await FacilityIssue.find({
-        createdBy: id
-      });
-      
-      res.json(facilityIssues);
-    }catch(err){
-      console.log(`There was an error fetching the facility reports for a specific user: ${err}`);
-    }
-  })
-  .get('/issue/:id', async (req: Request, res: Response) => {
-    try{
-      const facilityIssueId = req.params.id;
-      const facilityIssue = await FacilityIssue.findById(facilityIssueId);
-
-      res.json(facilityIssue);
-    }catch(err){
-      console.log(`There was an error fetching a facility report by report id: ${err}`);
-    }
-  })
-  .get('/apartment/:id', async (req: Request, res: Response) => {
-    try{
-      const apartmentId = req.params.id;
-      const facilityIssues = await FacilityIssue.find({ apartmentId });
-      
-      res.json(facilityIssues);
-    }catch(err){
-      console.log(`There was an error fetching the facility reports by apartment id: ${err}`);
-    }
-  })
-  .post('/create', async(req: Request, res: Response) => {
-    try{
-      // When auth is added, we should get the user info from the JWT token. For now, this is to simulate receiving a user id
-      const testUser = await EmployeeUser.findOne({ username: 'john.doe' });
-      const id = testUser?._id;
-      const { issueDetails }: { issueDetails: IssueDetails} = req.body
-
-      const facilityIssue = await FacilityIssue.create({
-        ...issueDetails,
-        createdBy: id,
-        apartmentId: testUser?.apartmentId
-      })
-
-      res.status(201).send(facilityIssue)
-    }catch(err){
-      console.log(`There was an error creating a facility issue: ${err}`)
-    }
-  })
-  .put('/comment/:facilityIssueId', async(req: Request, res: Response) => {
-    try{
-      const { facilityIssueId } = req.params;
-      const { comment }: { comment: string } = req.body;
-      // When auth is added, we should get the user info from the JWT token. For now, this is to simulate receiving a user id
-      
-      const testUser = await EmployeeUser.findOne({ username:'john.doe' });
-      const id = testUser?._id;
-
-      // Note: Because the issues are randomized in the seed, you should make sure to use facility issues (in the params) owned by the user when testing.
-      const facilityIssue = await FacilityIssue.findById(facilityIssueId);
-      
-      if(!comment) res.status(400).json('Comment cannot be empty');
-      else if(!facilityIssue) res.status(404).json('The facility issue cannot be found');
-      else if(facilityIssue.status === 'closed') res.status(409).json('Cannot update because issue is closed');
-      else if(!facilityIssue.createdBy.equals(id)) res.status(401).json('No permission to add a comment');
-      else{
-        const newComment = {
-          description: comment,
-          createdBy: id
-        };
-  
-        facilityIssue.comments.push(newComment);
-        await facilityIssue.save();
-  
-        res.json(facilityIssue);
-      }
-    }catch(err){
-      console.log(`There was an error updating the facility issue: ${err}`);
-    }
-  })
-  .put('/close/:facilityIssueId', async(req: Request, res: Response) => {
-    try{
-      const { facilityIssueId } = req.params;
-      // When auth is added, we should get the user info from the JWT token. For now, this is to simulate receiving a user id
-      const testUser = await EmployeeUser.findOne({ username:'john.doe' });
-      const id = testUser?._id;
-
-      // Note: Because the issues are randomized in the seed, you should make sure to use facility issues (in the params) owned by the user when testing.
-      const facilityIssue = await FacilityIssue.findById(facilityIssueId);
-
-      if(!facilityIssue) res.status(404).json('The facility issue was not found');
-      else if (!facilityIssue.createdBy.equals(id)) res.status(401).json('No permission to close the facility issue');
-      else{
-        facilityIssue.status = 'closed';
-        await facilityIssue.save();
-
-        res.json(facilityIssue);
-      };
-    }catch(err){
-      console.log(`There was an error when attempting to close the facility issue: ${err}`);
-    }
-  });
+  .get('/test', testFacilityRouter)
+  .get('/user', getIssuesForUser)
+  .get('/issue/:id', getFacilityIssue)
+  .get('/apartment/:id', getFacilityIssuesForApartment)
+  .post('/create', createFacilityIssue)
+  .put('/comment/:facilityIssueId', commentOnFacilityIssue)
+  .put('/close/:facilityIssueId', closeFacilityIssue);
 
 export default facilityIssueRouter;
--- server/src/api/routers/personalInfoRouter.ts
+++ server/src/api/routers/personalInfoRouter.ts
@@ -1,64 +1,12 @@
-import PersonalInfo, { PersonalInfoTypeT } from '../../models/PersonalInfo';
-import express, { Request, Response } from 'express';
+import { getPersonalInfo, testPersonalInfoRouter, updatePersonalInfo } from '../../controllers/personalInfo';
 
-import EmployeeUser from '../../models/EmployeeUser';
-import Onboarding from '../../models/Onboarding';
+import express from 'express';
 
 const personalInfoRouter = express.Router();
-// temp test user, await auth
-const currentTestUser = 'john.doe';
 
 personalInfoRouter
-  .get('/test', (_req: Request, res: Response) => {
-    try {
-      res.json('Successfully hit personal info router');
-    } catch (error) {
-      console.log(
-        `There was an error in the personal info test route: ${error}`
-      );
-    }
-  })
-  .get('/', async (_req: Request, res: Response) => {
-    try {
-      const user = await EmployeeUser.findOne({ username: currentTestUser });
-      if (!user) throw Error('User not found');
-
-      const onboarding = await Onboarding.findById(user.onboardingId);
-      if (!onboarding || onboarding.status !== 'approved') {
-        throw Error('User has not onboard yet');
-      }
-
-      const personalInfo = await PersonalInfo.findById(user.personalInfoId)
-        .select('+SSN')
-        .exec();
-      if (!personalInfo) throw Error('Failed to fetch personal information');
-
-      res.json(personalInfo);
-    } catch (error: unknown) {
-      console.log(`Error fetching personal information: ${error}`);
-    }
-  })
-  .put('/update', async (req: Request, res: Response) => {
-    try {
-      const { updates }: { updates: Partial<PersonalInfoTypeT> } = req.body;
-      const user = await EmployeeUser.findOne({ username: currentTestUser });
-      if (!user) throw Error('User not found');
-
-      const onboarding = await Onboarding.findById(user.onboardingId);
-      if (!onboarding || onboarding.status !== 'approved') {
-        throw Error('User has not onboard yet');
-      }
-
-      const updateInfo = await PersonalInfo.findByIdAndUpdate(
-        user.personalInfoId,
-        updates,
-        { new: true }
-      ).select('+SSN');
-
-      res.json(updateInfo);
-    } catch (error: unknown) {
-      console.log(`Error updating personal information: ${error}`);
-    }
-  });
+  .get('/test', testPersonalInfoRouter)
+  .get('/', getPersonalInfo)
+  .put('/update', updatePersonalInfo);
 
 export default personalInfoRouter;
