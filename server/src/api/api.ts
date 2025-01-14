af9f3ac6d9687a2f185ad69844a62f9f1f573f85
Duth Ng <rumduth1@gmail.com>
2025-01-14 05:54:09 -0600

    Finish SCRUM-2 SCRUM-17 SCRUM-19 SCRUM-21

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -1,18 +1,18 @@
-import apartmentRouter from './routers/apartmentRouter';
-import express from 'express';
-import facilityIssueRouter from './routers/facilityRouter';
-import onboardingRouter from './routers/onboardingRouter';
-import personalInfoRouter from './routers/personalInfoRouter';
-import userRouter from './routers/userRouter';
-import visaRouter from './routers/visaRouter';
+import apartmentRouter from "./routers/apartmentRouter";
+import express from "express";
+import facilityIssueRouter from "./routers/facilityRouter";
+import onboardingRouter from "./routers/onboardingRouter";
+import personalInfoRouter from "./routers/personalInfoRouter";
+import userRouter from "./routers/userRouter";
+import visaRouter from "./routers/visaRouter";
 
 const apiRouter = express.Router();
 
-apiRouter.use('/user', userRouter);
-apiRouter.use('/apartment', apartmentRouter);
-apiRouter.use('/onboarding', onboardingRouter);
-apiRouter.use('/visa', visaRouter);
-apiRouter.use('/facilityIssue', facilityIssueRouter);
-apiRouter.use('/personalInfo', personalInfoRouter);
+apiRouter.use("/user", userRouter);
+apiRouter.use("/apartment", apartmentRouter);
+apiRouter.use("/onboarding", onboardingRouter);
+apiRouter.use("/visa", visaRouter);
+apiRouter.use("/facilityIssue", facilityIssueRouter);
+apiRouter.use("/personalInfo", personalInfoRouter);
 
 export default apiRouter;
