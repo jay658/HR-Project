95ed84c82ce0a3e62cc219db1388c7e3bde48812
JerlsonSik <jlsonxyue@gmail.com>
2025-01-14 10:30:24 -0500

    14 Jan - 1030

--- server/src/api/routers/userRouter.ts
+++ server/src/api/routers/userRouter.ts
@@ -1,8 +1,7 @@
 import express from 'express'
 import {Request, Response} from 'express'
 import EmployeeUser from '../../models/EmployeeUser'
-const bcrypt = require('bcrypt');
-
+import bcrypt from 'bcryptjs'
 const userRouter = express.Router()
 
 // Test API
@@ -16,27 +15,28 @@ userRouter.get('/test', (_req, res) => {
 
 // Get all users
 userRouter.get('/allusers', async (req : Request, res : Response) : Promise<any> => {
-  const users = await EmployeeUser.find({}).sort({createdAt:-1})
+  const users = await EmployeeUser.find({}).sort({})
   return res.status(200).json(users)
 })
 
 // Register user
 userRouter.post('/register', async (req : Request, res : Response) : Promise<any> => {
-  const {username, email, password} = req.body
-
-  // Check if username exists
-  const usernameExists = await EmployeeUser.findOne({username: username});
-  if (usernameExists) {
-    return res.status(409).json({error : "User Already Exists"})
-  }
-
-  // Check if email exists
-  const emailExists = await EmployeeUser.findOne({email: email});
-  if (emailExists) {
-    return res.status(409).json({error : "Email Already Exists"})
-  }
-
   try{
+  
+    const {username, email, password} = req.body
+
+    // Check if username exists
+    const usernameExists = await EmployeeUser.findOne({username: username});
+    if (usernameExists) {
+      return res.status(409).json({error : "User Already Exists"})
+    }
+
+    // Check if email exists
+    const emailExists = await EmployeeUser.findOne({email: email});
+    if (emailExists) {
+      return res.status(409).json({error : "Email Already Exists"})
+    }
+
     //Create Hash Password
     const passwords = await bcrypt.hash(password, Number(process.env.SALT));
 
