8661fd414462ab687d133597d729ca4f2787f696
JerlsonSik <jlsonxyue@gmail.com>
2025-01-14 17:14:11 -0500

    14 Jan - 1714

--- react/src/components/RegistrationPage.tsx
+++ react/src/components/RegistrationPage.tsx
@@ -1,17 +1,41 @@
 import React, { useState } from "react";
 import { Box, TextField, Button, Typography } from "@mui/material";
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '../store/store';
+import { registerUser } from "../store/registrationSlice/registrationThunks";
 
 const RegistrationPage: React.FC = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+  
+  const dispatch = useDispatch<AppDispatch>();
+  const registrationState = useSelector((state: RootState) => state.registration);
 
-  const handleRegister = () => {
-    console.log("Registering user:", { username, password, email });
+  const handleRegister = async () => {
+    const registerData = {
+      username: username,
+      email: email,
+      password: password
+    }
+
+    const result = await dispatch(registerUser(registerData));
+
+
+    if(!result.hasOwnProperty('error')){
+      setUsername("");
+      setPassword("");
+      setEmail('')
+    }
   };
 
   return (
     <Box sx={{ p: 3, maxWidth: 500, mx: "auto", mt: 5 }}>
+      {registrationState.error && (
+        <Typography color="error" sx={{ mb: 2 }}>
+            {registrationState.error}
+        </Typography>
+      )}
       <Typography variant="h4" gutterBottom>
         Registration
       </Typography>
--- react/src/store/store.ts
+++ react/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counterSlice/counterSlice'
 import onboardingReducer from './onboardingSlice/onboardingSlice'
+import registrationReducer from './registrationSlice/registrationSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    onboarding: onboardingReducer
+    onboarding: onboardingReducer,
+    registration: registrationReducer
   },
 });
 
