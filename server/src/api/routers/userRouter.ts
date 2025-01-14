be391f4e032fc670993501392682a64e92ec184b
Jerlson Sik <144855178+JerlsonSik@users.noreply.github.com>
2025-01-14 18:58:47 -0500

    SCRUM 4, SCRUM-8, SCRUM-9, SCRUM-10 - user should be able to see visa status (#24)
    * commit1
    
    * commit 1
    
    * commit 2
    
    * commit 14 Jan - 00:14
    
    * 14 Jan - 1848

--- server/src/api/routers/userRouter.ts
+++ server/src/api/routers/userRouter.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import { testUserRouter } from '../../controllers/user'
+import { testUserRouter, getAllUserRouter, registerRouter} from '../../controllers/user'
 
 const userRouter = express.Router()
 
 userRouter.get('/test', testUserRouter)
+userRouter.get('/allusers', getAllUserRouter)
+userRouter.post('/register', registerRouter)
 
 export default userRouter
