30d3211e24d667adcafb54739e1f4c6f7cbcef1d
JerlsonSik <jlsonxyue@gmail.com>
2025-01-14 00:14:21 -0500

    commit 14 Jan - 00:14

--- server/src/api/routers/visaRouter.ts
+++ server/src/api/routers/visaRouter.ts
@@ -22,18 +22,6 @@ visaRouter.get('/allvisa', async (req : Request, res : Response) : Promise<any> 
   return res.status(200).json(visaApplication)
 })
 
-visaRouter.get('/status',async (req: Request, res: Response): Promise<any> => {
-  try{
-    const {username} = req.body
-
-    const user = await EmployeeUser.findOne({username: username});
-
-
-  }catch(err){
-    console.log(`There was an error for getting status: ${err}`)
-  }
-})
-
 // Create New Visa Application
 visaRouter.post('/create', async (req: Request, res: Response): Promise<any> => {
   try{
@@ -125,17 +113,64 @@ visaRouter.post('/upload', async (req : Request, res : Response) : Promise<any> 
     }
 
     if(type !== visa.nextStep){
-      return res.status(404).json({error: 'You have to upload file' + type})
+      return res.status(404).json({error: 'You have to upload file: ' + visa.nextStep})
     }
 
-    const document = await Document.create({userId: user._id, type: type, fileKey: fileKey, fileUrl:fileURL})
-    
+    // Get personal info
+    const personalInfo = await PersonalInfo.findOne({userId:user._id})
+    if(!personalInfo){
+      return res.status(404).json({error : "Personal Info not found"})
+    }
 
+    // Create Document
+    const document = await Document.create({userId: user._id, type: type, fileKey: fileKey, fileUrl:fileURL})
+
+    // Save Document._id into Personal Info
+    personalInfo.employment?.documents.push(document._id)
+
+    await personalInfo.save()
+
+    return res.status(200).json({personalInfo, document, user, visa})
     
 
   }catch(err){
     console.log(`There was an error in the visa test route: ${err}`)
+    return res.status(500).json({mssg:err})
   }
+
+})
+
+visaRouter.get('/status', async (req : Request, res : Response) : Promise<any> => {
+  const { username } = req.body;
+
+  // Find user
+  const user = await EmployeeUser.findOne({username : username});
+  if (!user) {
+    return res.status(404).json({error : "User not found"})
+  }
+
+  // Get personal info
+  const personalInfo = await PersonalInfo.findOne({userId:user._id})
+  if(!personalInfo){
+    return res.status(404).json({error : "Personal Info not found"})
+  }
+
+  const allDocuments = personalInfo.employment?.documents
+  const resultArray = []
+  if(allDocuments){
+    for(let i of allDocuments){
+      const singleDocument = []
+      console.log(i)
+      const document = await Document.findById(i)
+      console.log(document?.status)
+      singleDocument.push(i)
+      singleDocument.push(document?.status)
+      resultArray.push(singleDocument)
+    }
+  }
+  
+
+  return res.status(200).json({allDOcument : resultArray})
 
 })
 
