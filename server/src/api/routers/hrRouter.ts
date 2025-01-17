9e61df997d9660874be9697a02d8f50bbde2091c
Jiayu Zhang <jiayuzhangg@gmail.com>
2025-01-16 19:26:15 -0500

    HR can search for onboarding by status and update statuses

--- server/src/api/routers/hrRouter.ts
+++ server/src/api/routers/hrRouter.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import onboardingRouter from './hrRouters/onboardingRouter';
 import userRouter from './hrRouters/userRouter';
 
 const hrRouter = express.Router();
 
 hrRouter.use('/user', userRouter);
+hrRouter.use('/onboarding', onboardingRouter)
 
 export default hrRouter
