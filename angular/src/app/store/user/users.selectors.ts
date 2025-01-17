7c966f58222cb3f27bf88b55fb078d44369e12f6
Jiayu Zhang <jiayuzhangg@gmail.com>
2025-01-16 19:08:38 -0500

    Added angular to the app along with a template for ngrx

---
+++ angular/src/app/store/user/users.selectors.ts
@@ -0,0 +1,9 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+import { UserState } from './users.reducers';
+
+export const selectUserState = createFeatureSelector<UserState>('users');
+
+export const selectAllUsers = createSelector(selectUserState, (state) => state.users);
+export const selectLoading = createSelector(selectUserState, (state) => state.loading);
+export const selectError = createSelector(selectUserState, (state) => state.error);
