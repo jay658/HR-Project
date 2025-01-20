42434b08c6caf6e757a025659667957cf15799cb
Duth <96284452+rumduth@users.noreply.github.com>
2025-01-19 18:30:14 -0600

    Update hrRouter.ts
    I accidentally deleted my housingRouter

--- server/src/api/routers/hrRouter.ts
+++ server/src/api/routers/hrRouter.ts
@@ -3,6 +3,7 @@ import facilityIssueRouter from './hrRouters/facilityIssueRouter';
 import onboardingRouter from './hrRouters/onboardingRouter';
 import userRouter from './hrRouters/userRouter';
 import hiringRouter from './hrRouters/hiringRouter';
+import housingRouter from "./hrRouters/housingRouter";
 
 const hrRouter = express.Router();
 
@@ -10,5 +11,7 @@ hrRouter.use('/user', userRouter);
 hrRouter.use('/onboarding', onboardingRouter)
 hrRouter.use('/facilityIssue', facilityIssueRouter)
 hrRouter.use('/hiring', hiringRouter)
+hrRouter.use("/housing", housingRouter);
+
 
 export default hrRouter
